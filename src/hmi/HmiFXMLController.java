e613a48fa72b469af49b5dcaad1ce58e7c021b48
DimitrisR92 <www.dimitris92@gmail.com>
2021-11-16 22:31:24 +0200

    Fixed fullscreen detail bar

--- src/hmi/HmiFXMLController.java
+++ src/hmi/HmiFXMLController.java
@@ -5,17 +5,24 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class HmiFXMLController implements Initializable {
     
     @FXML
-    private Label label;
+    private HBox details;
+    @FXML
+    private SplitPane fileName;
+    @FXML
+    private SplitPane wordsLines;
+    @FXML
+    private SplitPane lastSaved;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        
     }    
     
 }
